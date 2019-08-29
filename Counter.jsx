ef4581e3cc277f2a3085efa14d145747539ddf2a
Yoav Vollansky <yoavxxyoav@gmail.com>
2019-08-29 13:10:50 +0100

    Create Counter.jsx

---
+++ Counter.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
+  styles = {
+    fontSize: 50,
+    fontWeight: "bold"
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <img src={this.state.imageUrl} />
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
