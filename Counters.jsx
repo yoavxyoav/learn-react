52aa35514785d0a4c88e9236ffecbfb4f6286843
Yoav Vollansky <yoavxxyoav@gmail.com>
2019-09-05 16:58:37 +0100

    Update Counters.jsx

--- Counters.jsx
+++ Counters.jsx
@@ -11,22 +11,41 @@ class Counters extends Component {
     ]
   };
 
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+
+  }
+
   handleDelete = (counterId) => {
     console.log('event handler called', counterId)
     const counters = this.state.counters.filter(c => c.id !== counterId)
     this.setState({ counters }) // code was simplified form this.setState({ counters : counters}) because key : value are the same!
   }
 
+  handleReset = () => {
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    })
+    this.setState({ counters });
+  }
+
   render() {
     return (
       <div>
+        <button
+          onClick={this.handleReset}
+          className="btn btn-primary btn-sm m-2">Reset
+        </button>
         {this.state.counters.map(counter => (
           < Counter
             key={counter.id} // used internally by React
             onDelete={this.handleDelete}
-            //value={counter.value}
-            //id={counter.id}
-            // the next line simplifies the previous two lines by just getting the entire component
+            onIncrement={this.handleIncrement}
             counter={counter}
           />
         ))}
